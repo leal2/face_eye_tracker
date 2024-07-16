e603a46926522dda949c1e4f4aeac812f254fde2
Jose Leal <94211835+leal2@users.noreply.github.com>
2024-07-16 17:58:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye.xml')
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+        roi_gray = gray[y:y+w, x:x+w]
+        roi_color =  frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) == ord('q'):
+        break;
+cap.release()
+cv.destroyAllWindows
